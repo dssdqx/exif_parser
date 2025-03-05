9682a657e8f55013de5e245b37c3c3838d36b7a7
dssdqx <dssdqx@gmail.com>
2025-03-05 16:25:29 +0400

    upd v1.1

--- gsd.py
+++ gsd.py
@@ -1,7 +1,7 @@
 
 camera_specs = {
-    'M3E': {'pixel_size': 0.0064,  'image_width': 5280, 'focal_length': 24},  # Модель M3E
-    'M3M': {'pixel_size': 0.0064, 'image_width': 5280, 'focal_length': 24}  # Модель M3M
+    'M3E': {'pixel_size': 0.0064,  'image_width': 5280, 'focal_length': 24}, 
+    'M3M': {'pixel_size': 0.0064, 'image_width': 5280, 'focal_length': 24}  
 }
 
 
@@ -13,7 +13,7 @@ def calculate_gsd(height, model):
     gsd = (height * specs['pixel_size']) / (specs['focal_length'])
     gsd_cm = round(gsd, 3)*100
     if gsd_cm <= 1:
-        print(f'\nGSD: {round(gsd_cm, 2)} cm\naverage height (relative): {round(height, 2)} meters\nmaybe need to use thinning')
+        print(f'\nGSD: {round(gsd_cm, 2)} cm\naverage height (relative): {round(height, 2)} meters\nmaybe need to use thinning OR ortho/photos downsize')
     else:
         print(f'\nGSD: {round(gsd_cm, 2)} cm\naverage height (relative): {round(height, 2)} meters')
 
